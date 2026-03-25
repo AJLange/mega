f7f4383c96e64da626ec6c7824b9c5424488dde3
Amanda Lange <litagemini@gmail.com>
2026-03-25 14:34:33 -0400

    new web views
    just stubbing these out

--- web/website/urls.py
+++ web/website/urls.py
@@ -7,14 +7,23 @@ so it can reroute to all website pages.
 """
 
 from django.urls import path
+from .views import index
 
 from evennia.web.website.urls import urlpatterns as evennia_website_urlpatterns
 
 # add patterns here
+from web.website.views import index
+
 urlpatterns = [
-    # path("url-pattern", imported_python_view),
-    # path("url-pattern", imported_python_view),
+    path("", index.EvenniaIndexView.as_view(), name="index"),
+    path("policies/", index.EvenniaIndexView.as_view(), name="policies"),
+    path("setting/", index.EvenniaIndexView.as_view(), name="setting"),
+    path("timeline/", index.EvenniaIndexView.as_view(), name="timeline")
+
 ]
 
 # read by Django
 urlpatterns = urlpatterns + evennia_website_urlpatterns
+
+
+
