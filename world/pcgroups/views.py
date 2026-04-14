ca305f98293a002b11862a8e4d03be799cd55c45
Amanda Lange <litagemini@gmail.com>
2026-04-14 12:33:25 -0400

    fixing model typo

--- world/pcgroups/views.py
+++ world/pcgroups/views.py
@@ -4,9 +4,7 @@ from evennia import ObjectDB
 
 def group(request):
     group_list = ObjectDB.objects.filter(db_typeclass_path__contains="PlayerGroup") # filter by Character typeclass, since ObjectDB contains all objects
-    squad_list = ObjectDb.objects.filter(db_typeclass_path__contains="Squad")
-
-
+    squad_list = ObjectDB.objects.filter(db_typeclass_path__contains="Squad")
 
 
     context = {
